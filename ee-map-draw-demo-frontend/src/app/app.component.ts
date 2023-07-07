2df9561175a8e898ce30e1aa5b52d45160640d87
Gennadii Donchyts <dgena@google.com>
2023-07-07 12:24:21 +0000

    added empty map and table components

--- ee-map-draw-demo-frontend/src/app/app.component.ts
+++ ee-map-draw-demo-frontend/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ee-map-draw-demo-frontend';
+
+  addNewAsset() {
+  }
 }
--- ee-map-draw-demo-frontend/src/app/app.module.ts
+++ ee-map-draw-demo-frontend/src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AssetsComponent } from './components/assets/assets.component';
+import { MapComponent } from './components/map/map.component';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssetsComponent,
+    MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas)
+  }
+}
